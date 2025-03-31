1ceaf4f32297a31dbf16df04e4075905ea12a4c5
Shubham Sawarkar <shubhamsawarkar@live.com>
2025-03-31 01:19:29 -0400

    Add files via upload

---
+++ disable-image-scaling.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: Disable WordPress Image Scaling
+ * Description: Prevents WordPress from automatically scaling large images upon upload.
+ * Version: 1.0
+ * Author: Shubham Sawarkar
+ * License: MIT
+ */
+
+if (!defined('ABSPATH')) exit; // Prevent direct access
+
+// Disable WordPress' automatic image scaling feature
+add_filter('big_image_size_threshold', '__return_false');
+
+// Disable additional resized image sizes
+add_filter('intermediate_image_sizes_advanced', function($sizes) {
+    return [];
+});
